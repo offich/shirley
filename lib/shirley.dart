298a4738cf7b80d1399694b80fd654de5fc0707c
ume-kun1015 <baymax.blackandwhite@gmail.com>
2025-02-12 22:22:59 +0900

    feat: implemented prototype

--- lib/shirley.dart
+++ lib/shirley.dart
@@ -1,121 +1,311 @@
+import 'package:code_builder/code_builder.dart';
+import 'package:devtools_extensions/api.dart';
+import 'package:devtools_extensions/devtools_extensions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:shirley/src/ui/components/dynamic_widget.dart';
+import 'package:shirley/src/ui/components/preset_button.dart';
+import 'package:syntax_highlight/syntax_highlight.dart';
+import 'package:dart_style/dart_style.dart';
 
-class Shirley extends StatelessWidget {
+extension ColorExtension on Color {
+  int get redValue {
+    return (r * 255).round();
+  }
+
+  int get greenValue {
+    return (g * 255).round();
+  }
+
+  int get blueValue {
+    return (b * 255).round();
+  }
+
+  int get alphaValue {
+    return (a * 255).round();
+  }
+
+  String get toHex {
+    return '#'
+        '${alphaValue.toRadixString(16).padLeft(2, '0')}'
+        '${redValue.toRadixString(16).padLeft(2, '0')}'
+        '${greenValue.toRadixString(16).padLeft(2, '0')}'
+        '${blueValue.toRadixString(16).padLeft(2, '0')}';
+  }
+}
+
+class Shirley extends HookWidget {
   const Shirley({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    final theme = useState<HighlighterTheme?>(null);
+    final code = useState('');
+    final highlighter = useState<Highlighter?>(null);
+    final highlightedCode = useState<TextSpan>(TextSpan());
+    final jsonString = useState('');
+
+    final backgroundColor = useState<Color>(Color.fromRGBO(131, 217, 119, 1));
+    final content = useState('');
+
+    final buttonTextEditingController =
+        useTextEditingController(text: '1.Normal Button');
+
+    useEffect(() {
+      WidgetsBinding.instance.addPostFrameCallback((_) async {
+        await Highlighter.initialize(['dart']);
+
+        theme.value = await HighlighterTheme.loadDarkTheme();
+
+        highlighter.value = Highlighter(
+          language: 'dart',
+          theme: theme.value!,
+        );
+
+        final elevatedButton = refer('ElevatedButton').newInstance(
+          [],
+          {
+            'child': refer('Text').newInstance([
+              literalString(buttonTextEditingController.text),
+            ]),
+            'style': refer('ElevatedButton.styleFrom').call([], {
+              'backgroundColor': refer('Colors.fromRGBO').call([
+                literalNum(backgroundColor.value.redValue),
+                literalNum(backgroundColor.value.greenValue),
+                literalNum(backgroundColor.value.blueValue),
+                literalNum(1)
+              ])
+            }),
+            'onPressed': Method((builder) => builder
+              ..lambda = true
+              ..body = Code('() {}')).closure,
+          },
+        );
+
+        final parameter = Parameter(
+          (builder) => builder
+            ..name = 'context'
+            ..type = refer('BuildContext'),
+        );
+
+        final methods = [
+          Method(
+            (builder) => builder
+              ..name = 'build'
+              ..requiredParameters.add(parameter)
+              ..body = Code('return ${elevatedButton.accept(DartEmitter())};')
+              ..returns = refer('Widget'),
+          ),
+        ];
+
+        final shirleyButtonClass = Class((builder) {
+          builder
+            ..name = 'ShirleyButton'
+            ..extend = refer('StatelessWidget')
+            ..methods.addAll(methods);
+        });
+
+        final formattedCode = DartFormatter(
+          languageVersion: DartFormatter.latestLanguageVersion,
+          indent: 2,
+        ).format('${shirleyButtonClass.accept(DartEmitter.scoped())}');
+
+        code.value = formattedCode;
+
+        highlightedCode.value = highlighter.value!.highlight(formattedCode);
+
+        jsonString.value = '''
+{
+  "type": "elevated_button",
+  "args": {
+    "child": {
+      "type": "text",
+      "args": {
+        "text": "${content.value}"
+      }
+    },
+    "style": {
+      "backgroundColor": "${backgroundColor.value.toHex}"
+    }
   }
 }
+''';
+      });
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+      return;
+    }, [backgroundColor.value, content.value]);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    useEffect(() {
+      if (theme.value == null) return;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        highlighter.value = Highlighter(
+          language: 'dart',
+          theme: theme.value!,
+        );
 
-  final String title;
+        highlightedCode.value = highlighter.value!.highlight(code.value);
+      });
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+      return;
+    }, [theme.value]);
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+    if (highlighter.value == null) {
+      return SizedBox.shrink();
+    }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+    return DevToolsExtension(
+      eventHandlers: {
+        DevToolsExtensionEventType.themeUpdate: (event) async {
+          if (event.data == null) return;
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          final newTheme = event.data!['theme'];
+          theme.value = newTheme == 'dark'
+              ? await HighlighterTheme.loadDarkTheme()
+              : await HighlighterTheme.loadLightTheme();
+        },
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            spacing: 24.0,
+            children: [
+              SizedBox(
+                height: 80,
+                child: Column(
+                  spacing: 8.0,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('Shirley'),
+                    Text('Button Generator'),
+                    Builder(builder: (context) {
+                      final presets = List<Widget>.filled(3, PresetButton());
+                      return Row(
+                        spacing: 16.0,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: presets,
+                      );
+                    }),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 400,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 16.0,
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: DefaultTabController(
+                        length: 2,
+                        child: Column(
+                          children: [
+                            TabBar(
+                              tabAlignment: TabAlignment.fill,
+                              tabs: [
+                                Tab(child: Text('Preview')),
+                                Tab(child: Text('Code')),
+                              ],
+                            ),
+                            const SizedBox(height: 24),
+                            Expanded(
+                              child: TabBarView(
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      DynamicWidget(
+                                          jsonString: jsonString.value)
+                                    ],
+                                  ),
+                                  SelectableText.rich(highlightedCode.value),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 7,
+                      child: DefaultTabController(
+                        length: 3,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            TabBar(
+                              tabAlignment: TabAlignment.fill,
+                              tabs: [
+                                Tab(child: Text('Quick Settings')),
+                                Tab(child: Text('Body')),
+                                Tab(child: Text('Main Text')),
+                              ],
+                            ),
+                            const SizedBox(height: 24),
+                            Expanded(
+                              child: TabBarView(
+                                children: [
+                                  Column(children: [
+                                    Row(children: [
+                                      Container(
+                                        color: backgroundColor.value,
+                                        width: 200,
+                                        height: 200,
+                                      ),
+                                      SizedBox(
+                                        width: 200,
+                                        child: TextField(
+                                          decoration: InputDecoration(
+                                            labelText: 'Content',
+                                            hintText: 'Normal Button',
+                                          ),
+                                          controller:
+                                              buttonTextEditingController,
+                                          onChanged: (value) {
+                                            content.value = value;
+                                          },
+                                        ),
+                                      )
+                                    ])
+                                  ]),
+                                  Text.rich(highlightedCode.value),
+                                  Column(children: [SizedBox.shrink()]),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('or Pick from Presets'),
+                ],
+              ),
+              Builder(builder: (context) {
+                final presets = List<Widget>.filled(48, PresetButton());
+                return Wrap(spacing: 8.0, runSpacing: 16.0, children: presets);
+              }),
+              ColorPicker(
+                pickerColor: backgroundColor.value,
+                onColorChanged: (newColor) {
+                  backgroundColor.value = newColor;
+                },
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
