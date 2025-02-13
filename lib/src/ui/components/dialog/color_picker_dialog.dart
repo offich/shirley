22534e1022b2d6068705c81fb346dd840073a5cc
ume-kun1015 <baymax.blackandwhite@gmail.com>
2025-02-13 11:34:36 +0900

    fix: add border radius

--- lib/src/ui/components/dialog/color_picker_dialog.dart
+++ lib/src/ui/components/dialog/color_picker_dialog.dart
@@ -16,6 +16,7 @@ Future<void> showColorPickerDialog(
             child: ColorPicker(
               pickerColor: pickerColor,
               onColorChanged: onColorChanged,
+              pickerAreaBorderRadius: BorderRadius.circular(4),
             ),
           ),
         ),
