1fe5114e0aa70d52ba590f3d539438648ee7d105
ume-kun1015 <baymax.blackandwhite@gmail.com>
2025-03-11 11:37:27 +0900

    chore: leave comment to provide doc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shirley/shirley.dart';
 
+/// Runs Shirley on DevToolsExtension
 void main() {
   runApp(const ShirleyDevToolsExtension());
 }
